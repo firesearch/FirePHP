280502b0c75c75f9fe579c4fa5ff83e6a6648099
Ariston Lorenzo <ariston.thomas@gmail.com>
2019-02-25 21:49:02 -0500

    Finished main page (index.php)
    Signed-off-by: Ariston Thomas Lorenzo <ariston.thomas@firesearch.co>

--- index.php
+++ index.php
@@ -25,3 +25,26 @@ $url_query = 'lang=' . $lang
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+  <html ng-app="FirePHP">
+    <head>
+      <title>FirePHP</title>
+      <style type="text/css">
+        body  {
+          font-family: <?php echo $right_font_family; ?>; font-size: <?php echo $font_size; ?>
+        }
+      </style>
+    </head>
+    
+    <frameset cols="<?php echo $cfgLeftWidth; ?>,*" rows="*">
+    <frame src="left.php3?<?php echo $url_query; ?>" name="nav" frameborder="1" />
+    <frame src="<?php echo (empty($db)) ? 'main.php3' : 'db_details.php3'; ?>?<?php echo $url_query; ?>" name="phpmain" />
+
+    <noframes>
+        <body bgcolor="#FFFFFF">
+            <p><?php echo $strNoFrames; ?></p>
+        </body>
+    </noframes>
+</frameset>
+
+</html>
+
